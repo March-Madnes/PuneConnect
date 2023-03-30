c1372361cc4d8d0bd80a70ed85f76d7937a315db
dahighsky <aakashuttkarsh29@gmail.com>
2023-03-30 15:40:35 +0530

    qr_code_update

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -44,9 +44,14 @@ class HomeScreen extends StatelessWidget {
     final uid = auth.currentUser?.uid;
     var aadhar;
     firestore.collection('users').doc(uid).get().then((ds) {
-      aadhar = ds.get('adhar');
+      if (ds.exists) {
+        if (ds.data()!.containsKey('adhar')) {
+          aadhar = ds.get('adhar');
+        }
+      }
     });
-    String myaadhar = aadhar as String;
+
+    // String myaadhar = aadhar as String;
 
     return Scaffold(
         appBar: AppBar(
@@ -81,7 +86,7 @@ class HomeScreen extends StatelessWidget {
                           Container(
                             height: 200,
                             child: QrImage(
-                              data: myaadhar,
+                              data: aadhar.toString(),
                               version: QrVersions.auto,
                               size: 200.0,
                             ),
