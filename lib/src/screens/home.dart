a733e3d35a2d455f4f5ba3276b6af3c31018becf
Harshal141 <harshalmukundapatil@gmail.com>
2023-09-10 00:28:23 +0530

    async in main

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -20,131 +20,142 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final String title = 'Pune Connect';
   bool is_issuedPassAdded = false;
-
   dynamic fAadhar;
+  late String uid;
 
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  void getIssuedPasses() async {
+  @override
+  void initState() {
+    super.initState();
+    initializeUserData();
+    getIssuedPasses();
+  }
+
+  Future<void> initializeUserData() async {
+    final user = auth.currentUser;
+    uid = user?.uid ?? "";
+
+    try {
+      final ds = await firestore.collection("users").doc(uid).get();
+      setState(() {
+        fAadhar = ds.data()?['adhar'];
+      });
+    } catch (e) {
+      print("Error fetching user data: $e");
+    }
+  }
+
+  Future<void> getIssuedPasses() async {
     if (is_issuedPassAdded) {
       return;
     }
 
-    QuerySnapshot querySnapshot = await firestore
-        .collection("passes")
-        .doc(uid)
-        .collection(DateFormat('yyyy-MM-dd').format(DateTime.now()))
-        .get();
+    final user = auth.currentUser;
+    if (user == null) {
+      return;
+    }
 
-    for (QueryDocumentSnapshot documentSnapshot in querySnapshot.docs) {
-      int index = documentSnapshot['index'] as int;
-      List<IssuedPass> passes = libraryInstance.allIssuedPasses;
-      bool found = false;
-      for (int i = 0; i < passes.length; i++) {
-        if (passes[i].passIndex == index) {
-          found = true;
-          break;
+    final uid = user.uid;
+
+    try {
+      final querySnapshot = await firestore
+          .collection("passes")
+          .doc(uid)
+          .collection(DateFormat('yyyy-MM-dd').format(DateTime.now()))
+          .get();
+
+      for (QueryDocumentSnapshot documentSnapshot in querySnapshot.docs) {
+        final index = documentSnapshot['index'] as int;
+        final passes = libraryInstance.allIssuedPasses;
+        final found = passes.any((pass) => pass.passIndex == index);
+
+        if (!found) {
+          libraryInstance.addPass(passIndex: index, issueDate: DateTime.now());
         }
       }
-      if (!found) {
-        libraryInstance.addPass(passIndex: index, issueDate: DateTime.now());
-      }
+    } catch (e) {
+      print("Error fetching issued passes: $e");
     }
-  }
 
-  @override
-  void initState() {
-    super.initState();
+    setState(() {
+      is_issuedPassAdded = true;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    var user = auth.currentUser!.displayName;
-    final uid = auth.currentUser?.uid;
+    final user = auth.currentUser?.displayName ?? "";
 
-    getData() async {
-      await firestore.collection("users").doc(uid).get().then((ds) {
-        setState(() {
-          fAadhar = ds.data()?['adhar'];
-        });
-      });
-    }
-
-    if (!is_issuedPassAdded) {
-      getIssuedPasses();
-      setState(() {
-        is_issuedPassAdded = true;
-      });
-    }
     return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.logout_outlined),
-              tooltip: 'Open shopping cart',
-              onPressed: () async {
-                await auth.signOut();
-                RouteStateScope.of(context).go('/login');
-              },
-            ),
-          ],
-        ),
-        body: Column(
-          children: [
-            Flexible(
-              child: Center(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        // ignore: prefer_const_constructors
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 16),
-                          child: Text('Hii $user'),
+      appBar: AppBar(
+        title: Text(title),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.logout_outlined),
+            tooltip: 'Open shopping cart',
+            onPressed: () async {
+              await auth.signOut();
+              RouteStateScope.of(context).go('/login');
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Flexible(
+            child: Center(
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 16),
+                        child: Text('Hi $user'),
+                      ),
+                      if (libraryInstance.allIssuedPasses.isNotEmpty)
+                        Container(
+                          height: 200,
+                          child: QrImage(
+                            data: fAadhar.toString(),
+                            version: QrVersions.auto,
+                            size: 200.0,
+                          ),
                         ),
-                        if (libraryInstance.allIssuedPasses.isNotEmpty)
-                          Container(
-                            height: 200,
-                            child: QrImage(
-                              data: fAadhar.toString(),
-                              version: QrVersions.auto,
-                              size: 200.0,
-                            ),
-                          ),
-                        if (libraryInstance.allIssuedPasses.isEmpty)
-                          Column(
-                            children: [
-                              const Text("No Passes Issued for the day"),
-                              Padding(
-                                padding: const EdgeInsets.all(20),
-                                child: FilledButton(
-                                  onPressed: () {
-                                    RouteStateScope.of(context).go('/authors');
-                                  },
-                                  child: const Text("Issue Pass"),
-                                ),
+                      if (libraryInstance.allIssuedPasses.isEmpty)
+                        Column(
+                          children: [
+                            const Text("No Passes Issued for the day"),
+                            Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: FilledButton(
+                                onPressed: () {
+                                  RouteStateScope.of(context).go('/authors');
+                                },
+                                child: const Text("Issue Pass"),
                               ),
-                            ],
-                          ),
-                      ],
-                    ),
+                            ),
+                          ],
+                        ),
+                    ],
                   ),
                 ),
               ),
             ),
-            Flexible(
-              child: IssuedPassList(
-                passes: libraryInstance.allIssuedPasses,
-                onTap: (issuedPass) {
-                  print("Hello");
-                },
-              ),
+          ),
+          Flexible(
+            child: IssuedPassList(
+              passes: libraryInstance.allIssuedPasses,
+              onTap: (issuedPass) {
+                print("Hello");
+              },
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
